125f86a67183c5694b2f6e6186659163d520ae63
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-01-02 11:28:46 -0800

    commiting models/User.js

---
+++ models/User.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose'
+
+const UserSchema = Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export default model('user', UserSchema)
