4df1f5c6385e60a5b7e219111b902de109426db6
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-01-03 08:27:29 -0800

    commiting routes/users validation results

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,23 @@
 import express from 'express'
 import User from '../models/User.js'
+import { body, validationResult } from 'express-validator'
 
 
 const router = express.Router()
 
 
 
-router.post('/', (req, res) => {
-    res.send(req.body)
+router.post('/', [
+    body('name', 'Name is required').not().isEmpty(),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password should be 6 or more characters').isLength({ min: 6 })
+] ,(req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    res.send('success')
 })
 
 export default router
