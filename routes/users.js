12ea5562557c3a25de4db0e3c9aba52f687debba
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-01-03 09:56:52 -0800

    commiting salts and registering to database

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,49 @@
 import express from 'express'
 import User from '../models/User.js'
 import { body, validationResult } from 'express-validator'
-
-
+import bcrypt from 'bcryptjs'
 const router = express.Router()
 
 
 
 router.post('/', [
-    body('name', 'Name is required').not().isEmpty(),
+    body('firstName', 'Name is required').not().isEmpty(),
+    body('lastName', 'Name is required').not().isEmpty(),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password should be 6 or more characters').isLength({ min: 6 })
-] ,(req, res) => {
+] ,async(req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
+        res.status(400).json({ errors: errors.array() })
     }
 
-    res.send('success')
+
+    const { firstName, lastName, email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email })
+
+        if (user) {
+            res.status(400).send({ msg: 'user already exists, logging.' })
+        } else {
+            user = new User({
+                firstName,
+                lastName,
+                email,
+                password
+            })
+
+            const salt = await bcrypt.genSalt(10)
+            user.password = await bcrypt.hash(password, salt)
+
+
+            await user.save()
+            res.send('user saved')
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send({ msg: 'there was an error' })
+    }
 })
 
 export default router
