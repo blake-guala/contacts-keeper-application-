296e49571df234ceb586a5be265ea2a14d8392bf
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-01-04 07:24:02 -0800

    commiting checking of user info and hashing of passwords users.js

--- routes/users.js
+++ routes/users.js
@@ -7,24 +7,25 @@ const router = express.Router()
 
 
 router.post('/', [
-    body('firstName', 'Name is required').not().isEmpty(),
-    body('lastName', 'Name is required').not().isEmpty(),
+    body('firstName', 'First name is required').not().isEmpty(),
+    body('lastName', 'Last name is required').not().isEmpty(),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password should be 6 or more characters').isLength({ min: 6 })
 ] ,async(req, res) => {
+
     const errors = validationResult(req)
+
     if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() })
-    }
-
-
+        return res.status(400).json({errors: errors.array()})
+    } 
+    
     const { firstName, lastName, email, password } = req.body
 
     try {
         let user = await User.findOne({ email })
 
         if (user) {
-            res.status(400).send({ msg: 'user already exists, logging.' })
+            return res.status(400).json({ msg: 'The user already exists, login.' })
         } else {
             user = new User({
                 firstName,
@@ -34,16 +35,20 @@ router.post('/', [
             })
 
             const salt = await bcrypt.genSalt(10)
+
             user.password = await bcrypt.hash(password, salt)
 
+            res.send('user registered')
 
-            await user.save()
-            res.send('user saved')
+            user.save()
         }
     } catch (error) {
         console.error(error.message);
-        res.status(400).send({ msg: 'there was an error' })
+        return res.status(400).json({ error: 'bad request'})
     }
+
+
+
 })
 
 export default router
