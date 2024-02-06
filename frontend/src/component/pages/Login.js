95500dd87dd27ece97261f2fe94d29438615c81b
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-02-06 04:28:49 -0800

    login inputs setup

--- frontend/src/component/pages/Login.js
+++ frontend/src/component/pages/Login.js
@@ -6,18 +6,23 @@ export const Login = () => {
         password: ''
     })
 
+    const { email, password } = user
+
+    const onChange = (e) => setUser({...user, [e.target.name] : e.target.value})
+
+
   return (
 <form className='container'>
   <h1 className='text-custom'>Login <i className="fa-solid fa-user"></i></h1>
   <hr />
   <div className="mb-3">
     <label htmlFor="email" className="form-label">Email address</label>
-    <input type="email" className="form-control" required/>
+    <input type="email" value={email} name='email' className="form-control input-custom" onChange={onChange} required/>
     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div className="mb-3">
     <label htmlFor="password" className="form-label">Password</label>
-    <input type="password" className="form-control" required />
+    <input type="password" value={password} name='password' className="form-control input-custom" onChange={onChange} required />
   </div>
   <button type="submit " className="btn btn-primary button-custom ">Submit</button>
   <p className='text-custom '>Don't have an account yet? <a className='nav-custom' href="/register">Register here</a></p>
