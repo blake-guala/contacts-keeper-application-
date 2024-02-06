005983bbfcef7bd25cf15e696ff53de4bf41fb1a
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-02-06 09:43:25 -0800

    AddContacts.js

--- frontend/src/component/contacts/AddContacts.js
+++ frontend/src/component/contacts/AddContacts.js
@@ -4,35 +4,53 @@ export const AddContacts = () => {
     const [register, setRegister] = useState({
         firstName: '',
         lastName: '',
+        phone: '',
         email: '',
-        password: '',
-        password2: ''
+        type: 'Personal'
     })
 
-    const { firstName,lastName, email, phone } = register
+    const { firstName,lastName, email, phone, type } = register
 
     const onChange = (e) => setRegister({...register, [e.target.name] : e.target.value})
+
+    const onSubmit = e => {
+      e.preventDefault()
+      console.log(firstName,lastName,email,phone,type)
+    }
+
   return (
     <div>
-    <form className='container' >
-  <h1 className='text-custom'>Add contacts <i className="fa-solid fa-user"></i></h1>
+    <form className='container' onSubmit={onSubmit}>
+  <h1 className='text-custom'>Add contacts <i className="fa fa-user-plus" aria-hidden="true"></i></h1>
   <hr />
   <div className="mb-3">
-    <label htmlFor="firstName" className="form-label">First name <i className="fa fa-user-plus" aria-hidden="true"></i></label>
+    <label htmlFor="firstName" className="form-label">First name </label>
     <input type="text" value={firstName} name='firstName' className="form-control input-custom" onChange={onChange} required/>
   </div>
   <div className="mb-3">
-    <label htmlFor="lastName" className="form-label">Last name <i className="fa-solid fa-user"></i></label>
-    <input type="lastName" value={lastName} name='password' className="form-control input-custom" onChange={onChange} required />
+    <label htmlFor="lastName" className="form-label">Last name </label>
+    <input type="text" value={lastName} name='lastName' className="form-control input-custom" onChange={onChange} />
   </div>
   <div className="mb-3">
     <label htmlFor="phone" className="form-label">Phone <i className="fa-solid fa-phone"></i></label>
     <input type="text" value={phone} name='phone' className="form-control input-custom" onChange={onChange} required/>
   </div>
   <div className="mb-3">
-    <label htmlFor="email" className="form-label">Email address <i className="fa fa-envelope" aria-hidden="true"></i></label>
+    <label htmlFor="email" className="form-label">Email <i className="fa fa-envelope" ></i></label>
     <input type="email" value={email} name='email' className="form-control input-custom" onChange={onChange} required/>
   </div>
+  <div className="form-check">
+  <input className="form-check-input" value='Personal' type="radio" checked={type === 'Personal'} name="type" onChange={onChange} />
+  <label className="form-check-label" htmlFor="Personal">
+    Personal
+  </label>
+</div>
+<div className="form-check">
+  <input className="form-check-input" value='Business' type="radio" checked={type === 'Business'} name="type"  onChange={onChange}/>
+  <label className="form-check-label" htmlFor="Business">
+    Business
+  </label>
+</div>
   <button type="submit " className="btn btn-primary button-custom ">Add Contact</button>
 </form>
     </div>
