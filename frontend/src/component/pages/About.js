f6144589ae2b13575bbb21c4afc7a7a68a3a95aa
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-02-10 10:02:58 -0800

    aboutpage

--- frontend/src/component/pages/About.js
+++ frontend/src/component/pages/About.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div>
+    <div className='container'>
         <h1 className='text-custom'>About the application</h1>
+        <p>This is a  full-stack application that store and keeps contacts, with each user Performing crud functions and each user have accessibity only to their contacts. 
+          it uses mongoDB to manage database.
+        </p>
     </div>
   )
 }
